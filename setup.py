56e5180fc4f0de600394701dec884c8ddf87f63e
Anton Tagunov <anton.tagunov@yandex.ru>
2014-02-04 19:41:37 +0400

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="metayaml",
-    version="0.1",
+    version="0.3",
     author="Anton Tagunov",
     packages=["metayaml"],
     package_data={'': ['test_files/*.yaml']},
