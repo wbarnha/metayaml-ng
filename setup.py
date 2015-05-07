34e67fa893c6671d0bb547af8bfb4a8e7495482c
Anton Tagunov <anton.tagunov@yandex.ru>
2015-05-07 10:49:45 +0300

    bump 0.15.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="metayaml",
-    version="0.15",
+    version="0.15.1",
     author="Anton Tagunov",
     packages=["metayaml"],
     package_data={'': ['test_files/*.yaml']},
